40245ae2f3ae0ee7624e8e47920188ea73c3eef5
Achal Jhawar <35405812+achaljhawar@users.noreply.github.com>
2023-09-09 15:11:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express()
 const validUrl =  require('valid-url')
-mongoose.connect('Add your mongodb api key here',{
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true, useUnifiedTopology: true
 })
 
