5519ebc2adca476365f1b2ac46d29ffb3f934e87
Achal Jhawar <35405812+achaljhawar@users.noreply.github.com>
2023-10-04 22:32:13 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,20 @@
+const passport = require('passport')
+var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const GOOGLE_CLIENT_ID = process.env.CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.CLIENT_SECRET;
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://tyni-bits.onrender.com/login/google/callback",
+    passReqToCallback: true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+passport.serializeUser(function(user, done){
+    done(null, user);
+})
+passport.deserializeUser(function(user, done){
+    done(null, user);
+})
