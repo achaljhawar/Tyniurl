e7ec96230dea1680d16c60a2774b0d17762fda69
Achal Jhawar <achaljhawar03@gmail.com>
2023-10-14 00:59:14 +0530

    updated dependencies

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express()
 const validUrl =  require('valid-url')
+require('dotenv').config()
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true, useUnifiedTopology: true
 })
